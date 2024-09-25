c70d2d8ff1c789368b72511e85000e242fe0cad2
Mohamed El attar <mohamadelattar99@gmail.com>
2024-09-25 12:27:34 +0100

    "Spring Security JWT"

---
+++ src/main/java/com/openclassroms/springsecurityjwt/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.openclassroms.springsecurityjwt.controller;
+
+import com.openclassroms.springsecurityjwt.service.JWTService;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    private JWTService jwtService;
+
+    public LoginController(JWTService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/login")
+    public String getToken(Authentication authentication) {
+        String token = jwtService.generateToken(authentication);
+        return token;
+    }
+}
---
+++ src/main/java/com/openclassroms/springsecurityjwt/controller/ResourceController.java
@@ -0,0 +1,12 @@
+package com.openclassroms.springsecurityjwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourceController {
+    @GetMapping("/")
+    public String getResource() {
+        return  "a value ... ";
+    }
+}
